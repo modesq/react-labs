18ec13ce0b9d1647c43b9d351c026ae213af99f2
mahmoud qunbor <modesq.99q@gmail.com>
2022-08-23 20:46:32 +0300

    almost there...

--- src/components/main.js
+++ src/components/main.js
@@ -1,15 +1,25 @@
 import React from "react";
 import HornedBeast from "./hornedBeast";
-import image1 from "./assets/Gemsbok.jpg"
+import Data from "./data.json";
+
+
+let dataEntry = Data.map(element =>
+    <HornedBeast
+        id={element._id}
+        img={element.image_url}
+        title={element.title}
+        description={element.description}
+        keyword={element.keyword}
+        horns={element.horns}
+    />
+);
+
 
 class Main extends React.Component {
+
     render() {
         return (
-            <div>
-                <HornedBeast title='Gemsbok' img = {image1} description='The gemsbok is a large antelope of genus Oryx. Also known as the South African oryx, the species is found in the southern African countries Botswana, Namibia, South Africa and Zimbabwe. It inhabits deserts, savannas and grasslands.' ></HornedBeast>
-                <HornedBeast title='Markhor' img= {require('./assets/Markhor.jpg')} description='The markhor is a member of the goat genus Capra found in Central Asia. It lives at high altitudes on mountainous, rocky terrain, and is present on both the Karakoram and Himalaya mountain ranges. The species is the national animal of Pakistan.' ></HornedBeast>
-                <HornedBeast title='American Bison / Buffalo' img= {require('./assets/American-Bison-Buffalo.jpg')} description='The American bison is one of North America’s iconic species. In terms of both weight and body length, this huge and powerfully-built species is the continent’s largest land animal. The heaviest-recorded American bison weighed 1,270 kg / 2,800 lb.; about the same as a modern compact family car.' ></HornedBeast>
-            </div>
+            dataEntry
         )
     }
 }
